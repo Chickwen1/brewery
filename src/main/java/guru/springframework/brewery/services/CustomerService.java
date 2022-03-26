b02611bfbc4e48f798a30d898826d259686bf679
kitsune <miss.iserah@gmail.com>
2022-03-26 01:36:58 -0500

    Initial Push - Controllers, models, and testing

---
+++ src/main/java/guru/springframework/brewery/services/CustomerService.java
@@ -0,0 +1,15 @@
+package guru.springframework.brewery.services;
+
+import guru.springframework.brewery.web.model.CustomerDto;
+
+import java.util.UUID;
+
+public interface CustomerService {
+    CustomerDto getCustomerById(UUID customerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void deleteById(UUID customerId);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+}
